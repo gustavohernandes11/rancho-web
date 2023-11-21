adef0964b0b7fe6a328998ce61e053781137d7e3
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-21 10:38:05 -0300

    feat: add go back router in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,41 @@
-import { SignOut, Cog } from "@styled-icons/fa-solid";
+import { SignOut, Cog, ArrowLeft } from "@styled-icons/fa-solid";
 import styled from "styled-components";
 import { IconButton } from "./IconButton";
 import { Span } from "./Span";
 import { Title } from "./Title";
 import { DesktopOnly } from "./utils/DesktopOnly";
+import { useRouter } from "next/navigation";
 
 export const Header = ({ title }: { title: string }) => {
+	const { back } = useRouter();
 	return (
 		<StyledHeader>
-			<Title>{title}</Title>
-			<Span>
+			<IconButton
+				onClick={back}
+				type="light"
+				icon={<ArrowLeft size={16} />}
+			/>
+			<Title textAlign="center">{title}</Title>
+			<JustifyEnd>
 				<DesktopOnly>
 					<Paragraph>account.exemple@gmail.com</Paragraph>
 					<IconButton type="light" icon={<SignOut size={16} />} />
 				</DesktopOnly>
 				<IconButton type="light" icon={<Cog size={16} />} />
-			</Span>
+			</JustifyEnd>
 		</StyledHeader>
 	);
 };
 
 const StyledHeader = styled.header`
-	display: flex;
+	display: grid;
+	grid-template-columns: 1fr 2fr 1fr;
+	grid-auto-flow: row;
 	grid-area: header;
 	align-items: center;
 	justify-content: space-between;
 	width: 100%;
 	height: 3.75rem;
-	padding-inline: 1rem;
 
 	@media (min-width: ${({ theme }) => theme.screen.laptop}) {
 		padding-inline: 1rem;
@@ -35,5 +43,11 @@ const StyledHeader = styled.header`
 `;
 const Paragraph = styled.p`
 	display: inline;
+	text-align: start;
 	padding-block: 1rem;
+`;
+const JustifyEnd = styled.span`
+	display: inline-flex;
+	gap: 0.5rem;
+	justify-self: end;
 `;
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,17 @@
 import styled, { css } from "styled-components";
+type ITitleProps = {
+	marginBottom?: string;
+	textAlign?: string;
+	children: string;
+};
 
-export const Title = ({ children, ...props }: any) => {
+export const Title = ({ children, ...props }: ITitleProps) => {
 	return <StyledTitle {...props}>{children}</StyledTitle>;
 };
 
-const StyledTitle = styled.h2<{ marginBottom: string }>`
-	${({ marginBottom }) => css`
+const StyledTitle = styled.h2<ITitleProps>`
+	${({ marginBottom, textAlign }) => css`
 		margin-bottom: ${marginBottom || "0"};
+		text-align: ${textAlign || "start"};
 	`}
 `;
