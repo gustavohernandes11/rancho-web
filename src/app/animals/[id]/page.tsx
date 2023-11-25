c632bc49fa7a2ce3007f9c89d311bf53ca8f5bc6
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-25 17:37:48 -0300

    feat: add info fields

--- src/app/animals/[id]/page.tsx
+++ src/app/animals/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Header } from "@/components/Header";
 import { Menu } from "@/components/Menu";
-import { AnimalInfoCard } from "@/components/AnimalInfoCard";
+import { AnimalInfoCard } from "@/components/AnimalTable/AnimalInfoCard";
 import { DesktopOnly } from "@/components/utils/DesktopOnly";
 import { Button } from "@/components/Button";
 import { Span } from "@/components/Span";
@@ -9,6 +9,10 @@ import { Aside } from "@/layout/Aside";
 import { Content } from "@/layout/Content";
 import { Container } from "@/layout/Container";
 import { PageLayout } from "@/layout/PageLayout";
+import { Title } from "@/components/Title";
+import { AnimalRow } from "@/components/AnimalTable/AnimalRow";
+import { BatchDropdown } from "@/components/BatchDropdown";
+import { TextArea } from "@/components/TextArea";
 
 export default function AnimalsPage() {
 	return (
@@ -21,6 +25,31 @@ export default function AnimalsPage() {
 						age={"3 anos"}
 						code={"123"}
 						gender={"F"}
+					/>
+					<Title marginBottom="0.5rem" marginTop="1rem" as="h3">
+						Lote
+					</Title>
+					<BatchDropdown
+						viewMode={true}
+						title={"Vacas de leite"}
+						animals={[]}
+					/>
+					<Title marginBottom="0.5rem" marginTop="1rem" as="h3">
+						Maternidade
+					</Title>
+					<AnimalRow viewMode={true} />
+					<Title marginBottom="0.5rem" marginTop="1rem" as="h3">
+						Paternidade
+					</Title>
+					<AnimalRow viewMode={true} />
+					<Title marginBottom="0.5rem" marginTop="1rem" as="h3">
+						Observação
+					</Title>
+					<TextArea
+						disabled={true}
+						value={
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+						}
 					/>
 				</Content>
 				<DesktopOnly>
